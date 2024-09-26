d62ebedbfa25677f71ec1a4c4b41b5b6b9ca7151
dmiweb <dimbons@mail.ru>
2024-09-26 21:51:39 +0300

    upd files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Timeline
 
-**GitHub Page** - 
+**GitHub Page** - https://dmiweb.github.io/timeline/
 
 [![Build status](https://ci.appveyor.com/api/projects/status/310q1jmm4pq4onar?svg=true)](https://ci.appveyor.com/project/dmiweb/timeline)
--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const  FileManagerPlugin  =  require ('filemanager-webpack-plugin');
 
 module.exports = {
   target: 'web',
@@ -49,12 +48,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css',
-    }),
-    new FileManagerPlugin({
-      events: {
-        // onStart: { delete: ['dist'] },
-        onEnd: { copy: [{ source: 'src/static', destination: 'dist' }] },
-      },
     }),
   ],
 };
